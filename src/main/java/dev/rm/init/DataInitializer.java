d0b06c2fe6359cc6c31c8ac1a44c6c249962a895
darsay8 <darsay.one@gmail.com>
2024-11-17 22:40:50 -0300

    Add data initialization for default users

--- src/main/java/dev/rm/init/DataInitializer.java
+++ src/main/java/dev/rm/init/DataInitializer.java
@@ -2,18 +2,43 @@ package dev.rm.init;
 
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.stereotype.Component;
+import org.springframework.transaction.annotation.Transactional;
 
-import jakarta.transaction.Transactional;
+import dev.rm.model.Role;
+import dev.rm.model.User;
+import dev.rm.repository.UserRepository;
+import dev.rm.utils.PasswordUtil;
 import lombok.extern.slf4j.Slf4j;
+import java.util.Arrays;
 
 @Slf4j
 @Component
 public class DataInitializer implements CommandLineRunner {
 
+    private final UserRepository userRepository;
+
+    public DataInitializer(UserRepository userRepository) {
+        this.userRepository = userRepository;
+    }
+
     @Override
     @Transactional
     public void run(String... args) {
-        log.info("Initializing data...");
-    }
 
+        User adminUser = User.builder()
+                .username("James")
+                .email("james@mail.com")
+                .password(PasswordUtil.hashPassword("123ABC"))
+                .role(Role.ADMIN)
+                .build();
+
+        User regularUser = User.builder()
+                .username("hank")
+                .email("hank@mail.com")
+                .password(PasswordUtil.hashPassword("123ABC"))
+                .role(Role.USER)
+                .build();
+
+        userRepository.saveAll(Arrays.asList(adminUser, regularUser));
+    }
 }
